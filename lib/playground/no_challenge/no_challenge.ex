bd757ad446372c00ce28e0c2437f13c7393410f7
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-06-08 00:33:26 +0600

    feat: seperate ascending and descending do_range

--- lib/playground/no_challenge/no_challenge.ex
+++ lib/playground/no_challenge/no_challenge.ex
@@ -1,17 +1,23 @@
 defmodule Playground.NoChallenge do
+  @spec range(integer, integer) :: list
   def range(n, n), do: [n]
-  def range(n, m) when n < m, do: []
+  def range(n, m) when n > m, do: do_descending_range(m, n)
+  def range(n, m) when n < m, do: do_ascending_range(m, n)
 
-  def range(n, m) do
-    do_range(n, m)
+  defp do_descending_range(n, m, list \\ [])
+
+  defp do_descending_range(n, n, list), do: [n | list]
+
+  defp do_descending_range(n, m, list) do
+    list = [n | list]
+    do_descending_range(n + 1, m, list)
   end
 
-  defp do_range(n, m, list \\ [])
+  defp do_ascending_range(n, m, list \\ [])
+  defp do_ascending_range(n, n, list), do: [n | list]
 
-  defp do_range(n, n, list), do: list ++ [n]
-
-  defp do_range(n, m, list) do
-    list = list ++ [n]
-    do_range(n - 1, m, list)
+  defp do_ascending_range(n, m, list) do
+    list = [n | list]
+    do_ascending_range(n - 1, m, list)
   end
 end
