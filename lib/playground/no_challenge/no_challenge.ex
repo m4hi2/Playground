3c398300b7d3492614788dd8df3025836f6c441d
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-06-08 00:55:25 +0600

    refac: single fucntion to handle both cases
    Both cases are:
    1. when first argument is greater the list will be in descending order
    2. when first argument is smaller the list will be in ascending order

--- lib/playground/no_challenge/no_challenge.ex
+++ lib/playground/no_challenge/no_challenge.ex
@@ -1,23 +1,18 @@
 defmodule Playground.NoChallenge do
   @spec range(integer, integer) :: list
   def range(n, n), do: [n]
-  def range(n, m) when n > m, do: do_descending_range(m, n)
-  def range(n, m) when n < m, do: do_ascending_range(m, n)
+  def range(n, m), do: do_range(m, n)
 
-  defp do_descending_range(n, m, list \\ [])
+  defp do_range(n, m, list \\ [])
+  defp do_range(n, n, list), do: [n | list]
 
-  defp do_descending_range(n, n, list), do: [n | list]
-
-  defp do_descending_range(n, m, list) do
+  defp do_range(n, m, list) do
     list = [n | list]
-    do_descending_range(n + 1, m, list)
-  end
 
-  defp do_ascending_range(n, m, list \\ [])
-  defp do_ascending_range(n, n, list), do: [n | list]
-
-  defp do_ascending_range(n, m, list) do
-    list = [n | list]
-    do_ascending_range(n - 1, m, list)
+    cond do
+      n < m -> do_range(n + 1, m, list)
+      n > m -> do_range(n - 1, m, list)
+      true -> do_range(n, m, list)
+    end
   end
 end
